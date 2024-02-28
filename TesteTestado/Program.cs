eb426a814dd6199785603af6697054a53aab791c
ALUNO <ALUNO@LABFPC21>
2024-02-28 10:07:32 -0300

    Adicionar arquivos de projeto.

---
+++ TesteTestado/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteTestado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b;
+
+            a = float.Parse(Console.ReadLine());
+            b = float.Parse(Console.ReadLine());
+
+            float r = a / b;
+
+            Console.WriteLine(String.Format("{0:F2}", r) + "\n");
+
+            Console.ReadKey();
+
+        }
+    }
+}
